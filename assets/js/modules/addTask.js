90f89e6bea6b4fe11fec5822be985df02ef09986
Julien Arnould <Arnould_julien@outlook.fr>
2023-05-15 14:42:04 +0200

    add object task

--- assets/js/modules/addTask.js
+++ assets/js/modules/addTask.js
@@ -1,11 +1,34 @@
 let tasksArray = []
+let taskObject = {}
+
 
 export function addTask(){
     const form = event.target.parentNode
     for(let input of form){
         if(input.nodeName.toLowerCase() == "textarea"){
-            tasksArray.push(input.value)
-            console.log(tasksArray)
-        }
-    }
+            const titleTask = input.value
+            
+             taskObject = {
+    title: titleTask,
+    status: false,
+    description: "",
+    createDate: "",
+    deadline:"",
 }
+        }   
+         
+    }
+
+
+tasksArray.push(taskObject);
+console.log(tasksArray)
+// function completerObject () {
+let date1 = Date()
+let date2 = Date.now 
+
+document.getElementById
+
+}
+
+console.log(taskObject)
+
